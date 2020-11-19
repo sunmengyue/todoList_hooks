3eba3e7ce08f98b18162135d87c9b21b03b7c021
Simon <bardrabbit@gmail.com>
2020-11-18 21:35:49 -0500

    add skeleton of the project

---
+++ src/TodoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+
+function TodoApp() {
+  return <h1>Todo App</h1>;
+}
+
+export default TodoApp;
